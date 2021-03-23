3654727b07efd0d59473d27a2f11f3b4f631ad95
Bruno Mengaldo <bruno.mengaldo@birdie.ai>
2021-03-22 21:47:01 -0300

    [main]: Implemented tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,123 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// class Square extends React.Component {
+//     render() {
+//       return (
+//         <button
+//             className="square"
+//             onClick={() => this.props.onClick()}
+//         >
+//             { this.props.value }
+//         </button>
+//       )
+//     }
+// }
+
+function Square (props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
+  
+class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            board: Array(9).fill(null),
+            xIsNext: true,
+        }
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.board[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        )
+    }
+
+    handleClick(i) {
+        const squares = this.state.board.slice()
+        if (squares[i] || this.calculateWinner(squares)) { return }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({ board: squares, xIsNext: !this.state.xIsNext })
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ]
+        for (let i of lines) {
+            const [a, b, c] = i
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a]
+            }
+        }
+        return null
+    }
+  
+    render() {
+        const winner = this.calculateWinner(this.state.board)
+        let status
+
+        if (winner) {
+            status = 'Game winner is: ' + winner
+        } else {
+            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+        }
+  
+        return (
+            <div>
+                <div className="status"> {status} </div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
+  
+class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<Game />,
+document.getElementById('root')
+) 
